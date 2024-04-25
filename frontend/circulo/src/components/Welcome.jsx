a6eb6f28f32449904524ba843d6055fc55e29275
Carlos Perez <cmperez0730@gmail.com>
2024-04-24 21:00:04 -0700

    Welcome Home intro page almost finished

--- frontend/circulo/src/components/Welcome.jsx
+++ frontend/circulo/src/components/Welcome.jsx
@@ -1,8 +1,18 @@
+import Button from 'react-bootstrap/Button';
+
 const Welcome = () => {
   return(
     <section className="welcome">
     <h1>Welcome to Circulo</h1>
-    <h2>Please Log In</h2>
+    <h3>A place where you can connect with people around communities. A place that makes it easy to share and discuss your ideas freely. A safe place for any type of people who want to explore.</h3>
+    <div className="mb-2">
+        <Button variant="primary" size="lg">
+          LOG IN
+        </Button>{' '}
+        <Button variant="secondary" size="lg">
+          SIGN UP
+        </Button>
+      </div>
     </section>
   )
 }
